4a4fb633109f327d00c9d6c5cf82665d67a8cfff
James Gill <james@thinkhuman.com>
2016-12-11 16:51:50 -0800

    Removed requirements note

--- card_deck.py
+++ card_deck.py
@@ -7,16 +7,6 @@ Author: James Gill
 """
 
 
-# REQUIREMENTS
-
-# Using Python, please write a set of classes that represent a deck of cards. 
-# Classes like "card" and "deck" might be appropriate- but it's entirely up to you. 
-# You should be able to "shuffle" the deck. 
-# You should also be able to draw cards from the deck one at a time until the deck is empty. 
-# Unit tests are required, and the code should be usable from other code. 
-# If you want to include a user interface, that's fine, but it's not required.
-
-
 from card_deck import *
 
 Start.start_game()
